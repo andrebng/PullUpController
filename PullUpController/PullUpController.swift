9b90006fb4faf2a431ab057407f30f38bed21d40
Andre Nguyen <nguyen.andres@gmail.com>
2019-03-11 15:17:14 +0100

    Update PullUpController.swift
    Swift 4.2 update

--- PullUpController/PullUpController.swift
+++ PullUpController/PullUpController.swift
@@ -478,7 +478,7 @@ extension UIViewController {
                                   constrainBottomToSafeAreaLayoutGuide: Bool = false,
                                   animated: Bool) {
         assert(!(self is UITableViewController), "It's not possible to attach a PullUpController to a UITableViewController. Check this issue for more information: https://github.com/MarioIannotta/PullUpController/issues/14")
-        addChildViewController(pullUpController)
+        addChild(pullUpController)
         pullUpController.setup(superview: view, initialStickyPointOffset: initialStickyPointOffset, constrainBottomToSafeAreaLayoutGuide: constrainBottomToSafeAreaLayoutGuide)
         pullUpController.pullUpControllerAnimate(
             action: .add,
@@ -503,9 +503,9 @@ extension UIViewController {
                 self?.view.layoutIfNeeded()
             },
             completion: { _ in
-                pullUpController.willMove(toParentViewController: nil)
+                pullUpController.willMove(toParent: nil)
                 pullUpController.view.removeFromSuperview()
-                pullUpController.removeFromParentViewController()
+                pullUpController.removeFromParent()
             })
     }
     
